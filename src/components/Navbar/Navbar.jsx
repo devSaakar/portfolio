50a5d76a7d58c562e42669fb818cc7be38089ce3
sakar <saakarchauhan@gmail.com>
2022-06-28 09:24:18 +0530

    added Navbar in portfolio.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+
+import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
+const links = ["home", "about", "contact", "work", "skills"];
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {links.map((link) => {
+          return (
+            <li className="app__flex p-text" key={`link-${link}`}>
+              <div className="app__link-hover"></div>
+              <a className="app__link" href={`#${link}`}>
+                {link}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease:'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className="app__navbar-links">
+              {links.map((link) => {
+                return (
+                  <li key={`mobile-link-${link}`}>
+                    <a className="app__link" onClick={() => setToggle(false)} href={`#${link}`}>
+                      {link}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
